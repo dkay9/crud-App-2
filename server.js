2282786b4d523111dbe510679cef5f906aec9a11
Kalu David <kaludavid411@gmail.com>
2024-09-25 13:57:27 +0100

    another commit

--- server.js
+++ server.js
@@ -10,16 +10,20 @@ MongoClient.connect(connectionString)
   .then(client => {
     const db = client.db('naruto-quotes')
     const quotesCollection = db.collection('quotes')
-    app.use(bodyParser.urlencoded({ extended: true }))
+    app.set('view engine', 'ejs')
+    app.use(express.urlencoded({ extended: true }))
+    app.use(express.static('public'))
+    app.use(express.json())
+
     app.get('/', (req, res) => {
         quotesCollection
            .find()
            .toArray()
            .then(results => {
             console.log(results)
+            res.render('index.ejs', { quotes: results })
         })
         .catch(error => console.error(error))
-        res.sendFile(__dirname + '/index.html')
     })
     app.post('/quotes', (req, res) => {
         quotesCollection
